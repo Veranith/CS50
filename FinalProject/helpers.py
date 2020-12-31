455a8edca3fe192aab2f77d14820c434fa8e94e8
Ryan Robinson <RRobinson@Mealsonwheelsofcheyenne.com>
2020-12-31 20:42:19 +0000

    Added Azure DB functions

--- FinalProject/helpers.py
+++ FinalProject/helpers.py
@@ -4,8 +4,6 @@ import urllib.parse
 
 from flask import redirect, render_template, request, session
 from functools import wraps
-from azure.keyvault.secrets import SecretClient
-from azure.identity import DefaultAzureCredential
 
 
 def apology(message, code=400):
@@ -58,12 +56,4 @@ def lookup(symbol):
         }
     except (KeyError, TypeError, ValueError):
         return None
-
-
-# https://docs.microsoft.com/en-us/azure/key-vault/secrets/quick-create-python?tabs=cmd
-def connectionString(KVName):
-    VaultURI = f"https://{KVName}.vault.azure.net/"
-    credential = DefaultAzureCredential(exclude_shared_token_cache_credential=True)
-    client = SecretClient(vault_url=VaultURI, credential=credential)
-    return client.get_secret('dbConnectionString').value
     
